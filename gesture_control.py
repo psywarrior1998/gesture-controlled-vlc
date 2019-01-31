e8648de74fed85f8ed0f19cae0d21e9a26191e68
Sanyam Sharma <sanyamsharma252@gmail.com>
2019-01-31 21:31:40 +0530

    v2.0

--- gesture_control.py
+++ gesture_control.py
@@ -2,7 +2,7 @@ import serial  # Serial imported for Serial communication
 import time  # Required to use delay functions
 import pyautogui
 
-ArduinoSerial = serial.Serial('com5', 9600)  # Create Serial port object called arduinoSerialData
+ArduinoSerial       = serial.Serial('com5', 9600)  # Create Serial port object called arduinoSerialData
 time.sleep(2)  # wait for 2 seconds for the communication to get established
 
 while 1:
